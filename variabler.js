040c19ca352c7b164475da4154de9b3420339827
andrewrasmussen <35759778+rasm93@users.noreply.github.com>
2018-01-24 12:36:48 +0100

    Øvelse: Variabler 2-9

---
+++ variabler.js
@@ -0,0 +1,79 @@
+// Øvelse: Variabler 2
+
+/*let navn = "Andrew";
+
+console.log(navn);*/
+
+
+// Øvelse: Variabler 3
+
+/*let alder = 24;
+
+console.log(alder);*/
+
+
+// Øvelse: Variabler 4
+
+/*let byNavn = "Roskilde";
+//console.log(bynavn);
+
+console.log(byNavn);*/
+
+
+// Øvelse: Variabler 5
+
+/*let x = 5;
+let y = 10;
+
+console.log(x + y);*/
+
+
+// Øvelse: Variabler 6
+
+/*let x = 5;
+let y = 10;
+let z = x + y;
+
+console.log(z);*/
+
+
+// Øvelse: Variabler 7
+
+/*let mitFornavn = "Andrew";
+let mitEfternavn = "Rasmussen";
+let minAlder = 24;
+
+console.log('Hej, jeg hedder ' + mitFornavn + ' ' + mitEfternavn+ ' og jeg er ' + minAlder + ' år');*/
+
+
+// Øvelse: Variabler 8
+
+/*let moms = 1.25;
+let prisUdenMoms = 400;
+let prisMedMoms = moms * prisUdenMoms;
+
+console.log(prisMedMoms);
+
+let prisUdenMoms2 = prisMedMoms / moms;
+
+console.log(prisUdenMoms2);*/
+
+
+// Øvelse: Variabler 9
+
+let forste = 10;
+let anden = 20;
+
+console.log(forste, anden);
+
+forste = forste + anden;
+       //10       20
+      //30
+anden = forste - anden;
+      //30       20
+     //10
+forste = forste - anden;
+       //30       10
+      //20 
+
+console.log(forste, anden);
