df6528b1bd5a11fdbe64ac7b55099e0c928fff09
andrewrasmussen <35759778+rasm93@users.noreply.github.com>
2018-01-24 14:47:51 +0100

    Øvelse: Arrays 2-7

---
+++ arrays.js
@@ -0,0 +1,40 @@
+// Øvelse: Arrays 2
+
+var frugter = ["Pære", "Æble", "Banan"];
+
+console.log(frugter);
+
+
+// Øvelse: Arrays 3
+
+console.log(frugter[1]);
+
+
+// Øvelse: Arrays 4
+
+frugter[0] = "Fersken"
+
+console.log(frugter);
+
+
+// Øvelse: Arrays 5
+
+console.log(frugter.length);
+
+
+// Øvelse: Arrays 6
+
+frugter[frugter.length] = "Appelsin"
+
+console.log(frugter);
+
+
+// Øvelse: Arrays 7
+
+var personer = [
+    ["Andrew", 24],
+    ["John", 50],
+    ["Jakob", 19]
+]
+
+console.log(personer);
