7e14c87c5a12a9b4524b9e93b83ae06e333ce488
andrewrasmussen <35759778+rasm93@users.noreply.github.com>
2018-01-24 15:04:56 +0100

    Øvelse: Betingede sætninger 2-6

---
+++ betingelser.js
@@ -0,0 +1,13 @@
+// Øvelse: Betingede sætninger 2
+
+//var sand = true;
+var sand = false;
+
+if (sand){
+    console.log("Hej verden");
+}
+
+
+// Øvelse: Betingede sætninger 3
+
+var randomNum = Math.floor(Math.random() * 9);
