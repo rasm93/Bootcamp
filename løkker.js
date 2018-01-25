5e2193c023e2ad8d6d3efa14174a39a69938969a
Andrew James Rasmussen <4741609@RTS-365.dk>
2018-01-25 21:20:26 +0100

    Øvelse: Løkker 2-8

--- løkker.js
+++ løkker.js
@@ -43,8 +43,28 @@
 
 // Øvelse: Løkker 5
 
-var uge = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
+// var uge = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
 
-for(var i = 0; i < uge.length; i++){
-    console.log(uge[i]);
-}
+// for(var i = 0; i < uge.length; i++){
+//     console.log(uge[i]);
+// }
+
+
+// Øvelse: Løkker 7
+
+// var navne = ["Andrew", "Morten", "Lars", "Jens", "Kenneth", "Peter", "Lone", "Lis"];
+
+// navne.forEach(function(navn){
+//     console.log(navn);
+// });
+
+
+// Øvelse: Løkker 8
+
+var kunstnere = ["Drake", "Kanye West", "Rihanna", "Justin Timberlake"];
+
+kunstnere.forEach(function(kunstner, index){
+    console.log(kunstner + " er nummer " + index);
+});
+
+// Øvelse: Løkker 9
