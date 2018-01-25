75f30085b3ca4cb2ed363010053de35b9280c408
andrewrasmussen <35759778+rasm93@users.noreply.github.com>
2018-01-25 11:07:12 +0100

    Øvelse: Operatorer 2-5

---
+++ operatorer.js
@@ -0,0 +1,50 @@
+// Øvelse: Operatorer 2
+
+var forste = 5;
+var anden = 10;
+
+//console.log(forste + anden);
+
+//console.log(forste - anden);
+
+//console.log(forste * anden);
+
+//console.log(forste / anden);
+
+//console.log(forste % anden);
+
+//forste++;
+//console.log(forste);
+
+//anden--;
+//console.log(anden);
+
+// Øvelse: Operatorer 3
+
+//forste += anden;
+//forste -= anden;
+//forste *= anden;
+//forste /= anden;
+//forste %= anden;
+
+//console.log(forste);
+
+
+// Øvelse: Operatorer 4
+
+/*if ( forste == 10 && anden == 10){
+    console.log("Betingelsen er sand");
+}
+else{
+    console.log("Betingelsen er falsk");
+}*/
+
+
+// Øvelse: Operatorer 5
+
+if ( forste == 10 || anden == 10){
+    console.log("Betingelsen er sand");
+}
+else{
+    console.log("Betingelsen er falsk");
+}
