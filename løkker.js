76b24477a5c0dd1e204923650fcd88cd69e6434d
andrewrasmussen <35759778+rasm93@users.noreply.github.com>
2018-01-25 13:16:00 +0100

    Øvelse: Løkker 2-5

---
+++ løkker.js
@@ -0,0 +1,50 @@
+// Øvelse: Løkker 2
+
+//var i = 0;
+
+/*while (i < 25){
+    i++;
+    console.log(i);
+}*/
+
+/*do{
+    i++;
+    console.log(i);
+
+} while (i < 25);*/
+
+// for (var i = 0; i <= 25; i++){
+//     console.log(i);
+// }
+
+
+// Øvelse: Løkker 3
+
+//var i = 25;
+
+// do{
+//     console.log("Denne løkke vil køre 1 gang");
+// } while (false);
+
+// do{
+//     console.log("Denne løkke vil køre " + i + " gange mere");
+//     i--;
+// } while (i > 0){
+//     console.log("Denne løkke er nu slut");
+// }
+
+
+// Øvelse: Løkker 4
+
+// for (var i = 2017; i >= 1917; i--){
+//     console.log(i);
+// }
+
+
+// Øvelse: Løkker 5
+
+var uge = ["Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
+
+for(var i = 0; i < uge.length; i++){
+    console.log(uge[i]);
+}
