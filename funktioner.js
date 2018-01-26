18c5d8b28240d467fbb2d21cdab06512cae3fa40
andrewrasmussen <35759778+rasm93@users.noreply.github.com>
2018-01-26 12:10:10 +0100

    Øvelse: funktioner 1-3

--- funktioner.js
+++ funktioner.js
@@ -10,15 +10,34 @@
 
 // Øvelse: funktioner 2
 
-function udregn(x, y){
-    var resultat = x * y;
-    if (isNaN(x) && isNaN(y)){
-        console.log("Angivet variabler, er ikke et tal");
-    }
-    else{
-        console.log(x + " gange med " + y + " er lig med " + resultat);
-    }
-}
+// function udregn(x, y){
+//     var resultat = x * y;
+//     if (isNaN(x) || isNaN(y)){
+//         console.log("Angivet variabler, er ikke et tal");
+//     }
+//     else{
+//         console.log(x + " gange med " + y + " er lig med " + resultat);
+//     }
+// }
+
+// udregn(10, 5);
 
 
 // Øvelse: funktioner 3
+
+function udregn(x, y){
+
+    if (x == undefined || y == undefined){
+        console.log("Manglende argumenter");
+    }
+    else{
+        if (isNaN(x) || isNaN(y)){
+            console.log("Angivet variabler, er ikke et tal");
+        } else{
+        let resultat = x * y;    
+        console.log(x + " gange med " + y + " er lig med " + resultat);
+        }
+    }
+}
+
+udregn(10,5);
